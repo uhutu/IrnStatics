86bf301a0857074565e341d9b8938e7b42d47aa8
liudongpu <liudongpu@hotmail.com>
2019-06-28 17:32:05 +0800

    c

---
+++ IrnStatics/IrnStatics.h
@@ -0,0 +1,13 @@
+//
+//  IrnStatics.h
+//  IrnStatics
+//
+//  Created by 刘流 on 2019/6/28.
+//  Copyright © 2019 uhutu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTRootView.h>
+@interface IrnStatics : NSObject
+
+@end
---
+++ IrnStatics/IrnStatics.m
@@ -0,0 +1,13 @@
+//
+//  IrnStatics.m
+//  IrnStatics
+//
+//  Created by 刘流 on 2019/6/28.
+//  Copyright © 2019 uhutu. All rights reserved.
+//
+
+#import "IrnStatics.h"
+
+@implementation IrnStatics
+
+@end
